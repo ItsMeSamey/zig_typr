ede8d2e55cfa9b542cb619b548e9539a537422c2
ItsMeSamey <sameychain5041@gmail.com>
2024-07-10 14:55:00 +0530

    testing-1

--- interfaces/notcurses.zig
+++ interfaces/notcurses.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const Options = @import("options.zig");
 
-const nc = @cImport({
+/// Import the notcurses headers
+pub const nc = @cImport({
   @cDefine("_GNU_SOURCE", {});
   @cInclude("notcurses/notcurses.h");
   @cInclude("notcurses/nckeys.h");
@@ -14,13 +15,15 @@ const nc = @cImport({
 test nc {
   const testing = std.testing;
   const win = nc.notcurses_init(null, nc.stdout).?;
-  const err = nc.notcurses_stop(win);
-  testing.expect(err == 0) catch unreachable;
+  testing.expect(0 == nc.notcurses_stop(win)) catch unreachable;
 }
 
+/// Error union returned by nost functions
 const NotcursesErrors = error{
   InitError,
   StdPlaneIsNull,
+  ReparentFailed,
+  PlaneCreationFailed,
   DeinitError,
 };
 
@@ -32,50 +35,75 @@ const NotcursesErrors = error{
   // _ = supported_styles | palette_size;
   // _ = canfade or canchangecolor or cantruecolor;
 
+/// the main notcurses struct
 var ncStruct: ?*nc.struct_notcurses = null;
+/// The standard plane
 var ncStdplane: ?*nc.struct_ncplane = null;
 
-fn init(ncOptions: ?*const nc.struct_notcurses_options) NotcursesErrors!void {
-  ncStruct = nc.notcurses_init(ncOptions, nc.stdout) orelse return NotcursesErrors.InitError;
+/// the Header Plane, to display typing speed/stats etc.
+var ncHeaderPlane: ?*nc.struct_ncplane = null;
+/// the Plance on which the words appear
+var ncTypingPlane: ?*nc.struct_ncplane = null;
+
+/// Initialize notcurses with given options
+/// use null to Initialize with defaults
+pub fn init(ncOptions: ?*const nc.struct_notcurses_options) NotcursesErrors!void {
+  ncStruct = nc.notcurses_core_init(ncOptions, nc.stdout) orelse return NotcursesErrors.InitError;
   ncStdplane = nc.notcurses_stdplane(ncStruct) orelse return NotcursesErrors.StdPlaneIsNull;
+
+  ncHeaderPlane = nc.ncplane_create(ncStdplane, &.{
+    .y = 0, .x = 0,
+    .rows = 1, .cols = 0,
+    .margin_b = 0, .margin_r = 0,
+    .name = null,
+    .resizecb = null,
+    .flags = nc.NCPLANE_OPTION_MARGINALIZED | nc.NCPLANE_OPTION_AUTOGROW,
+  }) orelse return NotcursesErrors.PlaneCreationFailed;
+
+  // ncHeaderPlane = nc.ncplane_reparent(ncHeaderPlane, ncHeaderPlane) orelse return NotcursesErrors.ReparentFailed;
+
+  ncTypingPlane = nc.ncplane_create(ncStdplane, &.{
+    .y = 1, .x = 0,
+    .rows = 0, .cols = 0,
+    .margin_b = 0, .margin_r = 0,
+    .name = null,
+    .resizecb = null,
+    .flags = nc.NCPLANE_OPTION_MARGINALIZED | nc.NCPLANE_OPTION_VSCROLL,
+  }) orelse return NotcursesErrors.PlaneCreationFailed;
 }
-fn deinit() NotcursesErrors!void {
+
+/// Deinitialize notcurses, you must call this or terminal will be messed up
+pub fn deinit() NotcursesErrors!void {
   if (nc.notcurses_stop(ncStruct) != 0) return NotcursesErrors.DeinitError;
 }
 
-const NotcursesUI = struct {
+
+pub const NotcursesUI = struct {
   options: *Options.InterfaceOptions,
 
   const Self = @This();
 
-
-
-  fn print(self: *Self) !void{
+  pub fn print(self: *Self, str: [*:0]const u8) !void{
     _ = self;
-    const str: [*:0]const u8 = "Hi mann";
-
-    _ = nc.ncplane_putstr(ncStdplane, str);
-    _ = nc.notcurses_render(ncStruct);
+    _ = str;
+    if (0 != nc.ncplane_putstr(ncTypingPlane, "HI, Dude")) return ;
+    _ = nc.ncpile_render(ncHeaderPlane);
     std.time.sleep(1000_000_000*1);
   }
-
-  fn initWords() void{
-  }
-
 };
 
-test {
-  NotcursesUI.init(null) catch undefined;
+test NotcursesUI {
+  init(null) catch undefined;
 
   var options: Options.InterfaceOptions = .{.allocator = null, };
   var ui = NotcursesUI{&options};
   var bi = NotcursesUI{&options};
 
-  bi.print() catch undefined;
-  bi.print() catch undefined;
-  bi.print() catch undefined;
-  // ui.print() catch undefined;
+  try bi.print();
 
-  ui.deinit() catch undefined;
+  std.testing.expect(0 == nc.ncplane_putstr(ncTypingPlane, "Hi mann"));
+  std.testing.expect(0 == nc.notcurses_render(ncStruct));
+
+  try ui.deinit();
 }
 
--- main.zig
+++ main.zig
@@ -1,7 +1,20 @@
 // zig run -lc -lnotcurses -lnotcurses-core -lnotcurses-ffi main.zig
 const std = @import("std");
+const NCUI = @import("interfaces/notcurses.zig");
+const Options = @import("interfaces/options.zig");
+
+fn interfaces() !void {
+  try NCUI.init(null);
+  var options: Options.InterfaceOptions = .{ .allocator = null, };
+  var ui = NCUI.NotcursesUI{ .options = &options };
+  try ui.print("hlo mann");
+}
 
 pub fn main() !void{
-  return ;
+  interfaces() catch |e| {
+    try NCUI.deinit();
+    return e;
+  };
+  try NCUI.deinit();
 }
 
